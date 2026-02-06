616ca79faed4ebb9cc6c96ec20a8858f9463012a
nicopai <nicopai06@gmail.com>
2026-02-06 21:11:01 +0000

    Update path

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 const CACHE_NAME = 'eurex-exam-v1';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
+  './',
+  './index.html',
+  './manifest.json',
   'https://unpkg.com/react@18/umd/react.production.min.js',
   'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
   'https://unpkg.com/@babel/standalone/babel.min.js'
